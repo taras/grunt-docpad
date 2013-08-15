2d0d63082a554e528a589f3982e15760e43d5e85
Taras Mankovski <tarasm@gmail.com>
2013-08-16 00:33:30 +0200

    Added docpad:generate task

--- tasks/init/grunt-docpad.js
+++ tasks/init/grunt-docpad.js
@@ -9,6 +9,7 @@
 'use strict';
 
 require( 'coffee-script' );
+var docpad = require( 'docpad' );
 
 module.exports = function(grunt) {
 
@@ -17,7 +18,18 @@ module.exports = function(grunt) {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({});
 
-    var docpadConfig = require( './docpad' )
+    var docpadConfig = require( './docpad' );
+
+    docpad.createInstance( docpadConfig, function( err, docpadInstance ) {
+      if ( err ) return grunt.fail.warn( err.stack );
+
+      docpadInstance.action( 'generate', function ( err, result ) {
+        if ( err ) return grunt.log.error( err.stack );
+
+        grunt.log.writeln( "Generate executed successfully" );
+      });
+    });
+
 
   });
 
